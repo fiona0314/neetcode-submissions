f7bdec0e3b2c2d95e99ea32261d34ff7acc55e70
fiona0314 <fionajiang0314@gmail.com>
2026-08-25 16:12:08 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.9
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'True'
+print(type(variable))
+variable = [10]
+print(type(variable))
